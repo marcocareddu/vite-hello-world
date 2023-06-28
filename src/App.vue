1c3ffd091ce7198bafa5cf84205e3dbe65f2280b
marcocareddu <saldumaccu@tuta.io>
2023-06-28 15:07:32 +0200

    VUE App - Added style

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,6 @@
 import AppTitle from './AppTitle.vue';
 import AppHero from './AppHero.vue';
 export default {
-  data() {
-    return {
-
-    }
-  },
   components: {
     AppHero,
     AppTitle,
@@ -15,8 +10,26 @@ export default {
 </script>
 
 <template>
-  <AppTitle />
-  <AppHero />
+  <div class="container">
+    <main>
+      <AppTitle />
+      <AppHero />
+    </main>
+  </div>
 </template>
 
-<style scoped></style>
+<style>
+/* CSS RESET */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+/* Container */
+.container {
+  max-width: 1000px;
+  margin: 0 auto;
+  text-align: center;
+}
+</style>
